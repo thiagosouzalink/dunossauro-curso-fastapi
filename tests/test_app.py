666b23e825a09a9ebc731d7e6f85cd0b9e390b4c
Thiago Souza <thiagolsmail@gmail.com>
2025-05-29 17:54:13 -0300

    Configuração do ambiente e hello world com testes

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    """
+    Esse teste tem 3 etapas (AAA)
+        - A: Arrange - Arranjo
+        - A: Act     - Executa a coisa (o SUT)
+        - A: Assert  - Garanta que A é A
+    """
+    # Arrange
+    client = TestClient(app)
+
+    # Act
+    response = client.get("/")
+
+    # Assert
+    assert response.json() == {"message": "Olá mundo!"}
+    assert response.status_code == HTTPStatus.OK
